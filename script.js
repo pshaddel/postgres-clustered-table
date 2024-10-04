ca7b35c54ff1420f51fda026285e53b80a4b9f22
Poorshad Shaddel <p.shaddel@gmail.com>
2024-10-04 13:30:43 +0000

    add changes of script

--- script.js
+++ script.js
@@ -17,30 +17,40 @@ const main = async () => {
     // insert data to tables
     // await insertDataToTables(normalProductTable, clustredProductTable, client);
 
+    // get the size of the tables
+    await getPostgresRecordSize(normalProductTable, client);
+    await getPostgresRecordSize(clustredProductTable, client);
+    await getPostgresSharedBufferSize(client);
+
     // setting the index
     await createIndex(normalProductTable, 'rating', client);
     await createIndex(clustredProductTable, 'rating', client);
     await setClusteredIndex(clustredProductTable, 'rating', client);
+    await runAnalyze(client);
+    // bring down the shared buffer size to 20MB
+    // await setPostgresSharedBufferSize(client, '20MB');
 
 
     console.log('Start Querying');
+    // run range queries on both tables
+    // await runRangeQueriesOnBothTables(client, true);
+    // await runRangeQueriesOnBothTables(client, false);
 
-    await getProductsInRange(clustredProductTable, 2, 3, client);
-    await getProductsInRange(normalProductTable, 2, 3, client);
-    await getProductsInRange(clustredProductTable, 3, 4, client);
-    await getProductsInRange(normalProductTable, 3, 4, client);
-    await getProductsInRange(clustredProductTable, 4, 5, client);
-    await getProductsInRange(normalProductTable, 4, 5, client);
+    // run range queries on both tables with explain
+    await runRangeQueriesOnBothTablesExplain(client, true);
+    await runRangeQueriesOnBothTablesExplain(client, false);
 
 };
 
 main().then(() => process.exit(0)).catch((error) => { console.error(error); process.exit(1); });
 
 /**
- * Table contains id, name, rating a decimal between 0 and 5
+ * Table contains id, name, rating a decimal between 0 and 5, and a primary key id, add a field that we put large json object in it
  */
 async function createProductTable(tableName, client) {
-    await client.query(`CREATE TABLE ${tableName} (id SERIAL PRIMARY KEY, name TEXT, rating DECIMAL(2, 1));`);
+    await client.query(`DROP TABLE IF EXISTS ${tableName};`);
+    await client.query(`CREATE TABLE ${tableName} (id SERIAL PRIMARY KEY, name TEXT, rating DECIMAL(2, 1), data JSONB);`);
+    console.log(`Created table ${tableName}`);
 }
 
 async function insertDataToTables(normalProductTable, clustredProductTable, client) {
@@ -49,41 +59,115 @@ async function insertDataToTables(normalProductTable, clustredProductTable, clie
 
 
     let currrentInsert = []
-    const rows = 200000;
+    const rows = 1000000;
+    const batchSize = 3000;
     for (let i = 0; i < rows; i++) {
         const rating = Math.random() * 5;
+        // a large json object
+        const data = {
+            name: `product ${i}`,
+            rating: rating,
+            description: Array(10000).fill('a').join(''),
+        };
         // batch each 1000 rows
-        currrentInsert.push(`('product ${i}', ${rating})`);
+        currrentInsert.push(`('product ${i}', ${rating}, '${JSON.stringify(data)}')`);
 
-        if (currrentInsert.length === 1000) {
-            await client.query(`INSERT INTO ${normalProductTable} (name, rating) VALUES ${currrentInsert.join(',')};`);
-            await client.query(`INSERT INTO ${clustredProductTable} (name, rating) VALUES ${currrentInsert.join(',')};`);
+        if (currrentInsert.length === batchSize) {
+            await client.query(`INSERT INTO ${normalProductTable} (name, rating, data) VALUES ${currrentInsert.join(',')};`);
+            await client.query(`INSERT INTO ${clustredProductTable} (name, rating, data) VALUES ${currrentInsert.join(',')};`);
             currrentInsert = [];
-            console.log(`Inserted ${i} rows - inserted percentage: ${i / rows * 100}%`);
+            console.log(`Inserted ${i} rows - inserted percentage: ${(i / rows * 100).toFixed(3)}%`);
         }
     }
 
-    await client.end();
 }
 
 async function createIndex(tableName, columnName, client) {
     try {
+        console.time(`Index ${tableName} ${columnName}`);
         await client.query(`CREATE INDEX ${tableName}_${columnName}_index ON ${tableName} (${columnName});`);
+        console.time(`Index ${tableName} ${columnName}`);
     } catch (error) {
         console.log(`Index already exists on ${tableName} ${columnName}`);
     }
 }
 
 async function setClusteredIndex(tableName, columnName, client) {
+    console.time(`CLUSTER ${tableName} USING ${tableName}_${columnName}_index`);
     await client.query(`CLUSTER ${tableName} USING ${tableName}_${columnName}_index;`);
+    console.timeEnd(`CLUSTER ${tableName} USING ${tableName}_${columnName}_index`);
 }
 
 
 async function getProductsInRange(tableName, minRating, maxRating, client) {
     const uniqueName = `${tableName} ${minRating} - ${maxRating}`;
-    console.time(uniqueName);
+    const time = Date.now();
     const res = await client.query(`SELECT * FROM ${tableName} WHERE rating BETWEEN ${minRating} AND ${maxRating};`);
-    console.timeEnd(uniqueName);
+    const timeDiff = Date.now() - time;
+    const timeDiffMilli = (timeDiff / 1000).toFixed(3);
     const rowsLength = res.rows.length;
-    console.log(`Found ${rowsLength} rows in range ${minRating} - ${maxRating}`);
+    const tableNameStringWith30Charachters = tableName.padEnd(30, ' ');
+    console.log(`${tableNameStringWith30Charachters} [${minRating} - ${maxRating}] ${timeDiffMilli} ms`);
+}
+
+async function getProductsInRangeExplain(tableName, minRating, maxRating, client) {
+    const res = await client.query(`EXPLAIN (analyze, buffers) SELECT * FROM ${tableName} WHERE rating BETWEEN ${minRating} AND ${maxRating};`);
+    let buffersSharedRead = res.rows.find(row => row && row['QUERY PLAN'] ? row['QUERY PLAN'].includes('Buffers: shared read') : false) || {};
+    buffersSharedRead = buffersSharedRead['QUERY PLAN'] || '';
+
+    console.log(`${tableName} [${minRating} - ${maxRating}] ${buffersSharedRead}`);
+}
+
+async function getPostgresRecordSize(tableName, client) {
+    const res = await client.query(`SELECT pg_size_pretty(pg_total_relation_size('${tableName}'));`);
+    console.log(`Table ${tableName} size: ${res.rows[0].pg_size_pretty}`);
+}
+
+async function runRangeQueriesOnBothTables(client, tableOrder) {
+    const rangeSize = 0.1;
+    const ranges = [];
+    for (let i = 0; i < 5; i++) {
+        ranges.push([rangeSize * i, rangeSize * (i + 1)]);
+    }
+    for (const range of ranges) {
+        if (tableOrder) {
+            await getProductsInRange('normal_product', range[0], range[1], client);
+            await getProductsInRange('clustred_product', range[0], range[1], client);
+        } else {
+            await getProductsInRange('clustred_product', range[0], range[1], client);
+            await getProductsInRange('normal_product', range[0], range[1], client);
+        }
+    }
+}
+
+async function runRangeQueriesOnBothTablesExplain(client, tableOrder) {
+    const rangeSize = 0.1;
+    const ranges = [];
+    for (let i = 0; i < 5; i++) {
+        ranges.push([rangeSize * i, rangeSize * (i + 1)]);
+    }
+    for (const range of ranges) {
+        if (tableOrder) {
+            await getProductsInRangeExplain('normal_product', range[0], range[1], client);
+            await getProductsInRangeExplain('clustred_product', range[0], range[1], client);
+        } else {
+            await getProductsInRangeExplain('clustred_product', range[0], range[1], client);
+            await getProductsInRangeExplain('normal_product', range[0], range[1], client);
+        }
+    }
+}
+
+async function runAnalyze(client) {
+    await client.query('ANALYZE;');
+}
+
+async function getPostgresSharedBufferSize(client) {
+    const res = await client.query(`SHOW shared_buffers;`);
+    console.log(`Shared buffer size: ${res.rows[0].shared_buffers}`);
+}
+
+async function setPostgresSharedBufferSize(client, size) {
+    const result = await client.query(`SET shared_buffers TO '${size}';`);
+    console.log(`Shared buffer size set to ${size}`);
+
 }
